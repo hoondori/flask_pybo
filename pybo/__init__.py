45a4e24b9b5df00b8ade3d7286a45f294864c63e
hoondori <hoondori@gmail.com>
2025-02-09 08:45:49 +0900

    마크다운

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -2,9 +2,8 @@ from flask import Flask
 from flask_migrate import Migrate, migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flaskext.markdown import Markdown
 import config
-
-
 
 naming_convention = {
     "ix": 'ix_%(column_0_label)s',
@@ -43,6 +42,9 @@ def create_app():
     # 필터
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
+
+    # 마크다운
+    Markdown(app, extensions=['nl2br','fenced_code'])
 
 
 

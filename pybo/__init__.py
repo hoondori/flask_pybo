94e5fff1d9a664cf5bf5d7f0bcb6302b392ee70f
hoondori <hoondori@gmail.com>
2025-02-09 17:40:21 +0900

    custom 404 error handling production에 file logging 적용

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate, migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -14,6 +14,9 @@ naming_convention = {
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
+
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 def create_app():
     app = Flask(__name__)
@@ -45,7 +48,8 @@ def create_app():
     # 마크다운
     Markdown(app, extensions=['nl2br','fenced_code'])
 
-
+    # error handler
+    app.register_error_handler(404, page_not_found)
 
 
     return app
